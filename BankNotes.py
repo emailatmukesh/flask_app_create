41b4d0f7109e38605d41dfaa0e7abcaafe73917a
emailatmukesh <73861067+emailatmukesh@users.noreply.github.com>
2022-01-25 10:58:37 +0530

    Add files via upload

---
+++ BankNotes.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 21:51:19 2020
+
+@author: win10
+"""
+from pydantic import BaseModel
+# 2. Class which describes Bank Notes measurements
+class BankNote(BaseModel):
+    H1 : float
+    L1 : float
+    O1 : float
+    C1 : float
+    V1 : float
+    HiLo1 : float
+    H2 : float
+    L2 : float
+    O2 : float
+    C2 : float
+    V2 : float
+    HiLo2 : float
+    H3 : float
+    L3 : float
+    O3 : float
+    C3 : float
+    V3 : float
+    HiLo3 : float
+    I1 : float
+    I2 : float
+    I3 : float
+    I4 : float
+    I5 : float
+    I6 : float
+    I7 : float
+    I8 : float
+    I9 : float
+    I10 : float
+    I11 : float
+    I12 : float
+    I13 : float
+    I14 : float
+    I15 : float
+    I16 : float
+    I17 : float
+    I18 : float
+    I19 : float
+    I20 : float
+    I21 : float
+    I22 : float
+    I23 : float
+    I24 : float
+    I25 : float
+    I26 : float
+    I27 : float
+    I28 : float
+    I29 : float
+    I30 : float
